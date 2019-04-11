f4f0386d17fdc756885da3676df76e293e05b0e8
Chris Leppanen <leppanen@squareup.com>
2019-04-11 12:08:33 -0700

    Do our best do defend against unfortunate serialized strings.

--- src/Decoder.php
+++ src/Decoder.php
@@ -15,13 +15,34 @@ final class Decoder implements DecoderInterface
             return [];
         }
 
-        preg_match_all('/(^|;|\})(\w+)\|/i', $encodedSessionData, $matchesarray, PREG_OFFSET_CAPTURE);
+        $serializeTypes = [];
+        // array
+        $serializeTypes[]  = 'a\:[0-9]+\:\{';
+        // string
+        $serializeTypes[]  = 's\:[0-9]+\:"';
+        // float
+        $serializeTypes[]  = 'd\:[0-9]+\.?[0-9]*';
+        // integer/resource
+        $serializeTypes[]  = 'i\:[0-9]+';
+        // null
+        $serializeTypes[] = 'N';
+        // boolean
+        $serializeTypes[] = 'b\:[0-1]';
+        // object O:<i>:"
+        $serializeTypes[] = 'O\:[0-9]+\:"';
+        // We can't support references even though PHP's native session encoder does
+        // since we deserialize each key in a separate step.
+        // $serializeTypes[] = 'r\:[0-9]+';
+
+        $serializeTypesString = '(?:' . implode('|', $serializeTypes) . ')';
+
+        preg_match_all('/(?:^|;|\})(\w+)\|' . $serializeTypesString . '/', $encodedSessionData, $matchesarray, PREG_OFFSET_CAPTURE);
 
         $decodedData = [];
 
         $lastOffset = null;
         $currentKey = '';
-        foreach ($matchesarray[2] as $value) {
+        foreach ($matchesarray[1] as $value) {
             $offset = $value[1];
             if (null !== $lastOffset) {
                 $valueText = substr($encodedSessionData, $lastOffset, $offset - $lastOffset);
